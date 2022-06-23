d312d0b5cef70f9915894f8a65e98065c7a5ab23
Kendall Whitman <kendallw@protonmail.com>
2022-06-23 13:31:27 +0000

    remove conditional and add new route

--- index.js
+++ index.js
@@ -5,22 +5,30 @@ const app = express();
 const port = 8005
 
 app.use(cors({
-  origin: ['https://dev.hamsearch.io', 'https://hamsearch.io'],
+  origin: [
+    'https://dev.hamsearch.io',
+    'https://hamsearch.io',
+    'http://localhost:3000/'
+  ],
 }));
+
+app.get('/hamdb/', (req, res) => {
+  res.send({ messages: { status: 'EMPTY' } });
+})
 
 app.get('/hamdb/:callsign', async (req, res) => {
   const { callsign } = req.params;
 
-  if ((/[a-z,A-Z,0-9]/).test(callsign) && !!callsign) {
-    try {
-      const response = await fetch(`http://api.hamdb.org/${callsign}/json/hamsearch`);
-      const data = await response.json();
-      res.send(data.hamdb);
-    } catch (err) {
-      res.send({ status: 'OTHER' });
-    }
-  } else {
-    res.send({ status: 'EMPTY' });
+  try {
+    const response = await fetch(`http://api.hamdb.org/${callsign}/json/hamsearch`, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      }
+    });
+    const data = await response.json();
+    res.send(data.hamdb);
+  } catch (err) {
+    res.send({ messages: { status: 'OTHER' } });
   }
 
 });
