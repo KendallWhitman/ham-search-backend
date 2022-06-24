6c846f85bc00153b7a9f11e7196819ec651db929
Kendall Whitman <kendallw@protonmail.com>
2022-06-24 11:15:46 -0500

    master: cleanup calls

--- index.js
+++ index.js
@@ -8,29 +8,30 @@ app.use(cors({
   origin: [
     'https://dev.hamsearch.io',
     'https://hamsearch.io',
-    'http://localhost:3000/'
+    'http://localhost:3000'
   ],
 }));
 
-app.get('/hamdb/', (req, res) => {
+app.get('/', (req, res) => {
   res.send({ messages: { status: 'EMPTY' } });
 })
 
-app.get('/hamdb/:callsign', async (req, res) => {
+app.get('/:callsign', async (req, res) => {
   const { callsign } = req.params;
+  const isValid = (/[a-z,A-Z,0-9]/).test(callsign);
 
-  try {
-    const response = await fetch(`http://api.hamdb.org/${callsign}/json/hamsearch`, {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      }
-    });
-    const data = await response.json();
-    res.send(data.hamdb);
-  } catch (err) {
-    res.send({ messages: { status: 'OTHER' } });
+  if (isValid) {
+    try {
+      const response = await fetch(`https://api.hamdb.org/${callsign}/json/hamsearch`);
+      const data = await response.json();
+
+      res.send(data.hamdb);
+    } catch (err) {
+      res.send({ messages: { status: 'OTHER' } });
+    }
+  } else {
+    res.send({ messages: { status: 'EMPTY' } });
   }
-
 });
 
 app.listen(port, () => {
