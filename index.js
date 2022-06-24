b17eda00eb7afe8a92dfb4a02a1a3faf149c1808
Kendall Whitman <kendallw@protonmail.com>
2022-06-24 12:31:37 -0500

    master: transform data before returning object

--- index.js
+++ index.js
@@ -13,24 +13,56 @@ app.use(cors({
 }));
 
 app.get('/', (req, res) => {
-  res.send({ messages: { status: 'EMPTY' } });
-})
+  res.send({ status: 'EMPTY' });
+});
 
 app.get('/:callsign', async (req, res) => {
   const { callsign } = req.params;
   const isValid = (/[a-z,A-Z,0-9]/).test(callsign);
+  
+  const licenseClasses = {
+    T: "Technician",
+    G: "General",
+    E: "Extra",
+  };
+
+  const licenseStatus = {
+    A: "Active",
+    E: "Expired",
+  };
 
   if (isValid) {
     try {
-      const response = await fetch(`https://api.hamdb.org/${callsign}/json/hamsearch`);
-      const data = await response.json();
+      const hamdbUrl = `http://api.hamdb.org/${callsign}/json/hamsearch`
+      const response = await fetch(hamdbUrl);
+      const { hamdb } = await response.json();
+      const data = {
+        ...hamdb.callsign,
+        licenseStatus: hamdb.callsign.status,
+        status: hamdb.messages.status,
+      };
 
-      res.send(data.hamdb);
+      const formattedData = {
+        address: data.addr1,
+        address2: `${data.addr2 && `${data.addr2},`} ${data.state && `${data.state}.`} ${data.zip}`,
+        callsign: data.call,
+        cls: licenseClasses[data.class] || data.class,
+        country: data.country,
+        expires: data.expires,
+        grid: data.grid,
+        lat: data.lat,
+        licenseStatus: licenseStatus[data.licenseStatus] || data.licenseStatus,
+        lon: data.lon,
+        name: `${data.fname} ${data.name}`,
+        status: data.status,
+      }
+
+      res.send(formattedData);
     } catch (err) {
-      res.send({ messages: { status: 'OTHER' } });
+      res.send({ status: 'ERROR' });
     }
   } else {
-    res.send({ messages: { status: 'EMPTY' } });
+    res.send({ status: 'EMPTY' });
   }
 });
 
